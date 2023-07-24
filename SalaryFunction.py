7241512e6d516d42b0111af553b9184d71142a21
Александр Владимирович Задрутский <49377326+A1171@users.noreply.github.com>
2023-07-25 00:46:21 +0700

    Add files via upload

--- SalaryFunction.py
+++ SalaryFunction.py
@@ -4,8 +4,10 @@ from tensorflow.keras.models import Sequential, Model
 from tensorflow.keras.layers import Dense, Input
 from tensorflow.keras.optimizers import Adam
 from keras import regularizers
-
-
+import keras
+import pickle
+import numpy as np
+"""
 def CreteModel():
     L2_1=0.0
     model_simple = Sequential()
@@ -16,11 +18,32 @@ def CreteModel():
     model_simple.add(Dense(1, activation='linear'))
     model_simple.compile(optimizer=Adam(learning_rate=1e-5), loss='mse', metrics=['mae'])
     return model_simple
-
-model=CreteModel()
+"""
+def CreateModel():
+  L2_1=0.0
+  inputs1 = keras.Input(shape=(39))
+  inputs2 = keras.Input(shape=(2000))
+  x1 = keras.layers.Dense(512, activation='relu')(inputs1)
+  x1 = keras.layers.Dense(512, activation='relu')(x1)
+  x2 = keras.layers.Dense(512, activation='relu')(inputs2)
+  x2 = keras.layers.Dense(256, activation='relu')(x2)
+  x = keras.layers.Concatenate()([x1,inputs2])
+  x = keras.layers.Dense(128, activation='relu',kernel_regularizer=regularizers.l2(L2_1))(x)
+  x = keras.layers.Dense(1, activation='linear',kernel_regularizer=regularizers.l2(L2_1))(x)
+  model=keras.Model(inputs=[inputs1,inputs2], outputs=x)
+  model.compile(optimizer=Adam(learning_rate=1e-5), loss='mse', metrics=['mae'])
+  return model
+def ScaleBackSalary(Y,Std):
+  Y1=np.power(2.719,Y*Std*2)
+  return Y1
+model=CreateModel()
 model.load_weights("Model_Salary_weights.h5")
-
-def process(CandidateData):
-    prd=model.predict(CandidateData)
+with open('vectorizer.pkl','rb') as f:
+  y_std,tfidf_job_type=pickle.load(f)
+  
+def process(CandidateData,JobDescription):
+    TFIDF_result=tfidf_job_type.transform([JobDescription])
+    prd=model.predict([CandidateData,TFIDF_result])
+    prd=ScaleBackSalary(prd[0],y_std)
     return prd
 
